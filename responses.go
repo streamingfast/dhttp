212195313a5b1b9d04afa6f4d6746f347cea12c2
Matthieu Vachon <matt@eoscanada.com>
2019-08-07 00:43:04 -0400

    Added WriteHTML to write using an `html/template` `*template.Template` object

--- responses.go
+++ responses.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"go.uber.org/zap"
@@ -51,6 +52,17 @@ func WriteJSONString(ctx context.Context, w http.ResponseWriter, json string) {
 	w.Header().Set("Content-Type", "application/json")
 	if _, err := w.Write([]byte(json)); err != nil {
 		logWriteResponseError(ctx, "failed writing text response", err)
+	}
+}
+
+func WriteHTML(ctx context.Context, w http.ResponseWriter, htmlTpl *template.Template, data interface{}) {
+	ctx, span := dtracing.StartSpan(ctx, "write HTML formatted response")
+	defer span.End()
+
+	w.Header().Set("Content-Type", "text/html")
+
+	if err := htmlTpl.Execute(w, data); err != nil {
+		logWriteResponseError(ctx, "failed writing HTML response", err)
 	}
 }
 
